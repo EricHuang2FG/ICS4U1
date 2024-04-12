ea1dd0427d34320146563c9a8933932342565b9d
Eric Huang <xilahuade@foxmail.com>
2024-04-12 12:12:50 -0400

    Sort Analysis

---
+++ Sort Analysis/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public long[][] runBubbleSortRandomDistribution(int[] array) {
+        long[][] data = new long[5][10];
+        for (int i = 1; i <= 5; i++) {
+            int size = (int) Math.pow(10, i);
+            for (int j = 1; j <= 11; j++) {
+                // Complete later
+            }
+        }
+        return data;
+    }
+
+}
